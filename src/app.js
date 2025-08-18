dad284526debf20f74f092fa53e31c62cac31678
Aditya Kumar Maddeshiya <102402234+aditya-4747@users.noreply.github.com>
2025-08-18 21:17:40 +0530

    Add CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -6,6 +7,10 @@ const app = express();
 app.use(express.json({limit: "16kb"}))
 app.use(express.static("./public"));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 app.use(cookieParser());
 
 // route import 
