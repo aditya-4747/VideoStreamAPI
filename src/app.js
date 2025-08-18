d6ca34166629d758d92e599337411895459abf9e
aditya-4747 <adityamaddeshiya470@gmail.com>
2025-08-18 20:32:16 +0530

    Remove CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -7,10 +6,6 @@ const app = express();
 app.use(express.json({limit: "16kb"}))
 app.use(express.static("./public"));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
 app.use(cookieParser());
 
 // route import 
